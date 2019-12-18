18fb5d41cc3a68f4b606944bc3b503d5c5b926f1
Tasnuva Haque <reya.ruet@gmail.com>
2019-12-18 11:57:17 -0800

    Add project files.

---
+++ QuizQuestion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize question with a 'int' type.
+            QuizQuestion<int> questionA = new QuizQuestion<int>
+                ("How many teeth do sharks use in a lifetime?", 30000, 30000, 20000, 10000, "none of the above");
+            questionA.ShowType();
+            questionA.ShowQ();
+            questionA.ShowOptions();
+            questionA.ShowA();
+
+            // Initialize question with a 'string' type.
+            QuizQuestion<string> questionB = new QuizQuestion<string>
+                ("What type of shark swims fastest?", "The short fin mako shark.", 
+                "The Great white shark", "The short fin mako shark.","Tiger shark", "Blue shark");
+            questionB.ShowType();
+            questionB.ShowQ();
+            questionB.ShowOptions();
+            questionB.ShowA();
+        }
+    }
+}
---
+++ QuizQuestion/QuizQuestion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    class QuizQuestion<T>
+    {
+        public T Answer { get; private set; } // Generic property.
+
+        public T Option1 { get; private set; } // Generic property.
+
+        public T Option2 { get; private set; } // Generic property.
+
+        public T Option3 { get; private set; } // Generic property.
+
+        public string Option4 { get; set; }   // String property.
+
+        private string Question { get; set; } // String property.
+
+        public QuizQuestion(string question, T answer, T option1, T option2, T option3, string option4) // Constructor.
+        {
+            Question = question;
+            Answer = answer;
+            Option1 = option1;
+            Option2 = option2;
+            Option3 = option3;
+            Option4 = option4;
+
+        }
+        public void ShowType() // Show data type.
+        {
+            Console.Write("The instance type is: ");
+            Console.WriteLine(typeof(T).ToString());
+        }
+
+        public void ShowQ() // Question & Answer
+        {
+            Console.WriteLine("Question: " + Question);
+        }
+
+        public void ShowOptions()
+        {
+            Console.WriteLine("a. "+Option1.ToString());
+            Console.WriteLine("b. "+Option2.ToString());
+            Console.WriteLine("c. "+Option3.ToString());
+            Console.WriteLine("d. "+Option4.ToString());
+        }
+        public void ShowA() // Question & Answer
+        {
+            Console.WriteLine("Answer: " + Answer.ToString() + "\n");
+        }
+    }
+    
+
+}
